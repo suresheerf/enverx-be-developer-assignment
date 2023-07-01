8edb46852d65a3c47e61110c2fe3117db7c9773a
ososweb_backend <ososweb@spaarksweb.com>
2023-07-01 10:54:07 +0530

    added cookie-parser module

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 const express = require("express");
 const morgan = require("morgan");
+const cookieParser = require("cookie-parser");
 
 const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
 app.use(morgan("dev"));
 
 module.exports = app;

c4fece574b00b711f7956a1e80551505ae739848
ososweb_backend <ososweb@spaarksweb.com>
2023-07-01 10:27:55 +0530

    added DB uri string cofiguration

--- config/config.js
+++ config/config.js
@@ -3,5 +3,6 @@ const CONFIG = {};
 
 CONFIG.NODE_ENV = process.env.NODE_ENV;
 CONFIG.PORT = process.env.PORT || 3000;
+CONFIG.DBURI = process.env.DBURI || "localhost:27019";
 
 module.exports = CONFIG;

a02630115bcc0b0f32760438be312da873ddaab7
ososweb_backend <ososweb@spaarksweb.com>
2023-07-01 10:55:18 +0530

    added getPosts by pagination route

--- controllers/postController.js
+++ controllers/postController.js
@@ -20,6 +20,36 @@ module.exports.getPost = catchAsync(async (req, res, next) => {
     return next(new appError("This post has been delete", 404));
   res.status(200).json({ message: "success", blogPost });
 });
+
+module.exports.getPosts = catchAsync(async (req, res, next) => {
+  const page = req.query.page ? Number(req.query.page) : 1;
+  const perPage = 20;
+  const skip = (page - 1) * perPage;
+
+  const sortBy = req.query.sortBy || "time";
+
+  const blogPosts = await Post.aggregate([
+    {
+      $match: {
+        status: "Active",
+      },
+    },
+    {
+      sort: {
+        _id: -1,
+      },
+    },
+    {
+      $skip: skip,
+    },
+    {
+      $limit: perPage,
+    },
+  ]);
+
+  res.status(200).json({ message: "success", blogPosts, page });
+});
+
 module.exports.updatePost = catchAsync(async (req, res, next) => {
   const postObj = {};
   if (req.body.content.trim()) postObj.content = req.body.content;
--- routes/postRoutes.js
+++ routes/postRoutes.js
@@ -1,8 +1,18 @@
 const express = require("express");
 const postController = require("./../controllers/postController");
+const { protect } = require("../middleware/protect");
 const router = express.Router();
 
-router.route("/").post(authController.createPost);
-router.route("/:id").get(authController.getPost);
+router
+  .route("/")
+  .post(protect, postController.createPost)
+  .get(postController.getPosts);
+router
+  .route("/:id")
+  .get(postController.getPost)
+  .put(postController.updatePost)
+  .delete(postController.updatePost);
+router.route("/like/:id").get(postController.likePost);
+router.route("/unlike/:id").get(postController.unlikePost);
 
 module.exports = router;

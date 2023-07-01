b63f9192342372e8d32c16318655a5043227a90d
ososweb_backend <ososweb@spaarksweb.com>
2023-07-01 16:48:04 +0530

    added host env

--- config/config.js
+++ config/config.js
@@ -3,6 +3,10 @@ const CONFIG = {};
 
 CONFIG.NODE_ENV = process.env.NODE_ENV;
 CONFIG.PORT = process.env.PORT || 3000;
-CONFIG.DBURI = process.env.DBURI || "localhost:27019";
+CONFIG.JWT_EXPIRES_IN = process.env.JWT_EXPIRES_IN;
+CONFIG.JWT_COOKIE_EXPIRES_IN = process.env.JWT_COOKIE_EXPIRES_IN;
+CONFIG.JWT_SECRET = process.env.JWT_SECRET;
+CONFIG.HOST = process.env.HOST;
+CONFIG.DBURI = process.env.DBURI || "mongodb://127.0.0.1:27017";
 
 module.exports = CONFIG;

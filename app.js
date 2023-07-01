6847e178f13fb6796d4d34bffa8c7f45381a6a4e
ososweb_backend <ososweb@spaarksweb.com>
2023-07-01 15:37:37 +0530

    added error handling methods,static file serving

--- app.js
+++ app.js
@@ -1,12 +1,16 @@
 const express = require("express");
 const morgan = require("morgan");
 const cookieParser = require("cookie-parser");
+const path = require("path");
+const appError = require("./utils/appError");
 
 const userRoutes = require("./routes/userRoutes");
 const postRoutes = require("./routes/postRoutes");
+const globalErrHandler = require("./controllers/errorController");
 
 const app = express();
 
+app.use(express.static(path.join(__dirname, "public")));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
@@ -14,5 +18,11 @@ app.use(morgan("dev"));
 
 app.use("/user", userRoutes);
 app.use("/posts", postRoutes);
+
+app.all("*", (req, res, next) => {
+  next(new appError(`can't find ${req.originalUrl} on this server!`, 404));
+});
+
+app.use(globalErrHandler);
 
 module.exports = app;
